88f8c7d9db5ebba54e4a9f6536c313ac97733a8b
Lee Jing Hui <jinghuilee0@gmail.com>
2020-11-15 17:39:22 +0800

    testing

--- app/src/main/java/com/example/mr_framer_grocer/MainActivity.kt
+++ app/src/main/java/com/example/mr_framer_grocer/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.mr_framer_grocer
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mr_framer_grocer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/mr_framer_grocer/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/mr_framer_grocer/ProductDetailsActivity.kt
@@ -2,10 +2,40 @@ package com.example.mr_framer_grocer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mr_framer_grocer.databinding.ActivityProductDetailsBinding
 
 class ProductDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
+
+        binding.plusBtn.setOnClickListener{ plusQty() }
+        binding.minusBtn.setOnClickListener{ minusQty() }
+
+    }
+
+    private fun plusQty(){
+        val stringInQty = binding.qtyrect.text.toString()
+        var qty = stringInQty.toInt()
+
+        qty++
+
+        binding.qtyrect.text = qty.toString()
+
+    }
+
+    private fun minusQty(){
+        val stringInQty = binding.qtyrect.text.toString()
+        var qty = stringInQty.toInt()
+
+        qty--
+
+        binding.qtyrect.text = qty.toString()
+
     }
 }
+
+

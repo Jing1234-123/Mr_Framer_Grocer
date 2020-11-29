8cf3665591a127cea0776d74b14d4779436b9433
Your Name <nonameteam2000@gmail.com>
2020-11-29 16:30:14 +0800

    meisiew commit

--- app/src/main/java/com/example/mr_framer_grocer/MainActivity.kt
+++ app/src/main/java/com/example/mr_framer_grocer/MainActivity.kt
@@ -5,38 +5,34 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mr_framer_grocer.Model.Product
+import kotlinx.android.synthetic.main.activity_bottom_nav_bar.*
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_all_category)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(1).isEnabled = false
 
+        //intent = Intent(this, productList::class.java)
+        //startActivity(intent)
 
+        //       val button: Button = findViewById(R.id.button2)
+ //       button.setOnClickListener{
 
-        val button: Button = findViewById(R.id.button2)
-        button.setOnClickListener{
+ //           val product = Product(
+ //               "Petai", "RM 11.00", "(±200g)",
+ //               "https://mrfarmergrocer.com/wp-content/uploads/2020/05/pet.jpg"
+//            )
 
-            val product = Product(
-                "Petai", "RM 11.00", "(±200g)",
-                "https://mrfarmergrocer.com/wp-content/uploads/2020/05/pet.jpg"
-            )
-
-            intent = Intent(this, ProductDetailsActivity::class.java)
-            intent.putExtra("name", product.name)
-            intent.putExtra("price", product.price)
-            intent.putExtra("weight", product.weight)
-            intent.putExtra("img", product.img)
-            startActivity(intent)
-        }
-
-
-
-//        intent = Intent(this, Testing::class.java)
-//        startActivity(intent)
-
+ //           intent = Intent(this, ProductDetailsActivity::class.java)
+   //         intent.putExtra("name", product.name)
+ //           intent.putExtra("price", product.price)
+//            intent.putExtra("weight", product.weight)
+//            intent.putExtra("img", product.img)
+//            startActivity(intent)
+//        }
 
 
     }

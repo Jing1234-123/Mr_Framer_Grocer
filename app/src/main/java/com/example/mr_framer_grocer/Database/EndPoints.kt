50295c63cf63a52771e0c216a4daec3a37763bc4
Lee Jing Hui <jinghuilee0@gmail.com>
2020-12-01 18:26:20 +0800

    jh commit

--- app/src/main/java/com/example/mr_framer_grocer/Database/EndPoints.kt
+++ app/src/main/java/com/example/mr_framer_grocer/Database/EndPoints.kt
@@ -5,6 +5,7 @@ object EndPoints {
         val URL_READ_R_PROD = URL_ROOT + "product/read_category.php"
         val URL_VERIFY_USER = URL_ROOT + "user/read_one.php"
         val URL_CREATE_USER = URL_ROOT + "user/create.php"
+        val URL_UPDATE_USER = URL_ROOT + "user/update.php"
 
     }
 

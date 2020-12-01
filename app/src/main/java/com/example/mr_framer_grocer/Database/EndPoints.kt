3f7e49c85d12a13aa2988ea99c10cb953ad47294
chialingg <74589719+chialingg@users.noreply.github.com>
2020-12-01 16:10:53 +0800

    commit by cl

--- app/src/main/java/com/example/mr_framer_grocer/Database/EndPoints.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mr_framer_grocer.Database
-
-object EndPoints {
-        private val URL_ROOT = "https://mr-farmer-grocer.000webhostapp.com/api/"
-        val URL_READ_R_PROD = URL_ROOT + "product/read_category.php"
-
-    }
-
